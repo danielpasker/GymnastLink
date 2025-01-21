76550b3a8541f6ad25d6983384e9a0744c898526
shay <shay5nis@gmail.com>
2025-01-21 17:35:09 +0200

    add registry and login

--- app/src/main/java/com/example/gymnastlink/ui/LoginActivity.kt
+++ app/src/main/java/com/example/gymnastlink/ui/LoginActivity.kt
@@ -3,18 +3,65 @@ package com.example.gymnastlink.ui
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gymnastlink.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailText: EditText
+    private lateinit var passwordText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<Button>(R.id.registerButton).setOnClickListener{
+        auth = Firebase.auth
+        emailText = findViewById(R.id.loginMailTextEdit)
+        passwordText = findViewById(R.id.loginPasswordTextEdit)
+
+        findViewById<Button>(R.id.registerButton).setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            login()
+        }
+    }
+
+    private fun goToApp() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun login() {
+        auth.signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,
+                        getString(R.string.authentication_succeeded),
+                        Toast.LENGTH_SHORT,).show()
+
+                    goToApp()
+                } else {
+                    Toast.makeText(baseContext,
+                        getString(R.string.authentication_failed),
+                        Toast.LENGTH_SHORT,).show()
+                }
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null) {
+            goToApp()
         }
     }
 }
--- app/src/main/java/com/example/gymnastlink/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/gymnastlink/ui/RegisterActivity.kt
@@ -1,21 +1,53 @@
 package com.example.gymnastlink.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.gymnastlink.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailText: EditText
+    private lateinit var passwordText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        auth = Firebase.auth
+        emailText = findViewById(R.id.registerMailTextEdit)
+        passwordText = findViewById(R.id.registerPasswordTextEdit)
+
+        findViewById<Button>(R.id.signInButton).setOnClickListener{
+            signIn()
         }
+    }
+
+    private fun signIn(){
+        auth.createUserWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.sign_in_successfully),
+                        Toast.LENGTH_SHORT,).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.authentication_failed),
+                        Toast.LENGTH_SHORT,).show()
+                }
+            }
     }
 }
