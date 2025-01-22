7e21e2ca9d0e680a5eb806bcbc79e9c5945d137c
shay <shay5nis@gmail.com>
2025-01-22 15:29:23 +0200

    combine login and registry activities

--- app/src/main/java/com/example/gymnastlink/ui/LoginActivity.kt
+++ app/src/main/java/com/example/gymnastlink/ui/LoginActivity.kt
@@ -7,6 +7,8 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gymnastlink.R
+import com.example.gymnastlink.controller.UserController
+import com.example.gymnastlink.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -26,12 +28,18 @@ class LoginActivity : AppCompatActivity() {
         passwordText = findViewById(R.id.loginPasswordTextEdit)
 
         findViewById<Button>(R.id.registerButton).setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+            register()
         }
 
         findViewById<Button>(R.id.loginButton).setOnClickListener {
             login()
+        }
+    }
+
+     override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null) {
+            goToApp()
         }
     }
 
@@ -58,10 +66,43 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-    public override fun onStart() {
-        super.onStart()
-        if (auth.currentUser != null) {
-            goToApp()
+    private fun register() {
+        auth.createUserWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.registered_successfully),
+                        Toast.LENGTH_SHORT,).show()
+
+                    saveNewUserInDb()
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.authentication_failed),
+                        Toast.LENGTH_SHORT,).show()
+                }
+            }
+    }
+
+    private fun saveNewUserInDb() {
+        val newUser = Firebase.auth.currentUser?.let {
+            User(
+                userId = it.uid,
+                userName = it.email.toString(),
+                userTitle = it.email.toString(),
+                age = 0.0,
+                weight = 0.0,
+                gender = "M/F",
+                height = 0.0,
+                userImg = ""
+            )
+        }
+
+        if (newUser != null) {
+            UserController.shared.insert(newUser){
+                goToApp()
+            }
         }
     }
 }
